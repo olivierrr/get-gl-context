5cbe37db6945ce042bdeccef360ca73dca432c34
olivierrr <oliviersinbox@gmail.com>
2014-10-16 16:09:30 -0700

    better error handling

--- webGL-utils.js
+++ webGL-utils.js
@@ -20,7 +20,7 @@
                 var canvas = document.createElement( 'canvas' )
                 return !!window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
             } catch (e) {
-                return false
+                return null
             }
         })()
     }
@@ -39,7 +39,8 @@
         gl.shaderSource(shader, source)
         gl.compileShader(shader)
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            return warn("could not compile " + type + " shader:\n\n" + gl.getShaderInfoLog(shader))
+            warn("could not compile " + type + " shader:\n\n" + gl.getShaderInfoLog(shader))
+            return null
         } else {
             return shader
         }
@@ -53,35 +54,41 @@
      * @param {String} vertexShader - shader source
      * @return {gl-context} - returns context with program attached to it (gl.program)
      */
-    function getGLprog(canvas, fragmentShader, vertexShader) {
+    function getGLprog(canvas, fs, vs) {
 
-        if (!glExists()) return warn('Can\'t get GL context!')
-        if (!fragmentShader) return warn('fragmentShader was not defined')
-        if (!vertexShader) return warn('vertexShader was not defined')
+        if (!glExists()) warn('Can\'t get GL context!')
+        else if (!canvas) warn('canvas was not defined')
+        else if (typeof fs !== 'string') warn('fragmentShader was not defined or isn\'t a string')
+        else if (typeof vs !== 'string') warn('vertexShader was not defined or isn\'t a string')
+        else {
 
-        // get gl context
-        var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+            // get gl context
+            var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
 
-        var compiled_vs = compileShader(gl, 'vertex', vertexShader)
+            var compiled_vs = compileShader(gl, 'vertex', vs)
+            var compiled_fs = compileShader(gl, 'fragment', fs)
 
-        var compiled_fs = compileShader(gl, 'fragment', fragmentShader)
+            if(!compiled_fs || !compiled_vs) return null
 
-        gl.program = gl.createProgram()
-        gl.attachShader(gl.program, compiled_vs)
-        gl.attachShader(gl.program, compiled_fs)
-        gl.linkProgram(gl.program)
+            gl.program = gl.createProgram()
+            gl.attachShader(gl.program, compiled_vs)
+            gl.attachShader(gl.program, compiled_fs)
+            gl.linkProgram(gl.program)
 
-        if (!gl.getProgramParameter(gl.program, gl.LINK_STATUS)) {
-            warn('could not link the shader program!')
-            gl.deleteProgram(gl.program)
-            gl.deleteProgram(compiled_vs)
-            gl.deleteProgram(compiled_fs)
-            return false
-        } else {
-            // install program to current rendering state
-            gl.useProgram(gl.program)
-            return gl
+            if (!gl.getProgramParameter(gl.program, gl.LINK_STATUS)) {
+                warn('could not link the shader program!')
+                gl.deleteProgram(gl.program)
+                gl.deleteProgram(compiled_vs)
+                gl.deleteProgram(compiled_fs)
+                return null
+            } else {
+                // install program to current rendering state
+                gl.useProgram(gl.program)
+                return gl
+            }
         }
+
+        return null
     }
 
     root.getGLprog = getGLprog
